07f6e700e6dfa3139b4fdff484ebe357daa64a50
Mannn <mannpatel060805@gmail.com>
2024-12-21 18:21:29 +0530

    Modified enum and mixin file

--- 14_mixin.dart
+++ 14_mixin.dart
@@ -1,28 +1,32 @@
 void main(){
-  duck().posi();
+  member mem = member();
+  mem.introduce();
+  mem.role();
 }
 
-class animal{
-  void posi(){
-    print("changed position");
+// Defining an abstract class to serve as an interface for club members
+abstract class clubMember{
+  String get name;
+  void introduce();
+}
+
+// Defining a mixin to provide additional functionality for club members
+mixin Role{
+  void role(){
+    print("Organizing an event.");
+  }
+  void greetings(){
+    print("Hello there.");
   }
 }
 
-mixin canfly{
-  void fly(){
-    print("by flying.");
-  }
-}
+class member with Role implements clubMember{
+  //Need to be implemented
+  @override
+  String get name => "XYZ";
 
-mixin canswim{
-  void swim(){
-    print("by swimming.");
-  }
-}
-
-class duck extends animal with canfly, canswim{
-  void posi(){
-    super.posi();
-    fly();
+  @override
+  void introduce(){
+    print("Hi I am ${name}");
   }
 }
--- 15_enums.dart
+++ 15_enums.dart
@@ -1,20 +1,49 @@
 void main(){
 
-  Car myCar = Car(carStyle: carType.sports);
-  print(myCar.carStyle);
+  Role member1 = Role(posi: clubPosition.Heads, name: "Mann");
+  print(member1);
+  Role member2 = Role(posi: clubPosition.values[1], name: "Devarsh");
+  print(member2);
 
 }
 
-class Car{
+class Role{
 
-  carType carStyle;
+  String name;
+  clubPosition posi;
 
-  Car({required this.carStyle});
+  // Constructor to initialize the name and position
+  Role({required this.posi, required this.name});
+
+  @override
+  String toString(){
+    String result = "Name: ";
+    result += name;
+
+    // Use a switch-case to append the role based on the position
+    switch (posi) {
+      case clubPosition.Executives:
+        result += ", Role: Executive";
+        break;
+      case clubPosition.Heads:
+        result += ", Role: Head";
+        break;
+      case clubPosition.Members:
+        result+= ", Role: Member";
+        break;
+      default:
+        print("Error"); // Handles unexpected position
+        break;
+    }
+
+    return result;
+  }
 
 }
 
-enum carType{
-  hatchback,
-  Suv,
-  sports,
-}
+// Enum to define possible positions in the club
+enum clubPosition{
+  Executives,
+  Heads,
+  Members,
+} 
